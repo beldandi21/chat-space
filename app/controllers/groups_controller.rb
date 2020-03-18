50270acc4d7434265b0e1a16ffbcaba3c134975e
beldandi21 <beldandi21@gmail.com>
2020-03-18 23:08:35 +0900

    インクリメンタルサーチの実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,6 +24,7 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+  
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?) and id !=(?)', "%#{params[:keyword]}%", current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
 
   def edit
+
   end
 
   def update
