8978ebc2d14a6e1f2b6920e327903fa311612262
beldandi21 <beldandi21@gmail.com>
2020-03-22 18:18:22 +0900

    fixed01

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-<<<<<<< Updated upstream
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-=======
-  resources :groups, only: [:new, :create, :edit, :update] do
->>>>>>> Stashed changes
     resources :messages, only: [:index, :create]
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
